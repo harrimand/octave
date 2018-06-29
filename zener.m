b3f4e7a85a863432ba05289d4aedb88ca6ab092c
harrimand <harrimand@gmail.com>
2018-06-28 20:13:21 -0700

    Update MatLab Compatible

--- zener.m
+++ zener.m
@@ -18,13 +18,13 @@ function  [RsMin, RsMax] = zener(Vs, Vz, IL, PzMax)
   IzMax = PzMax / Vz;    % Max Zener Current
   V_Rs = Vs - Vz;        % Series Resistor Voltage
   I_tMax = IL + IzMax;   % Maximum Total Current
-  RsMin = V_Rs / I_tMax; % Minimum Series Resistor Value
-  RsMax = V_Rs / IL;     % Maximum Series Resistor Value
+  RsMin = V_Rs / I_tMax; % Minimum Series Resistor Value (Max Zener Current)
+  RsMax = V_Rs / IL;     % Maximum Series Resistor Value (No Zener Current)
   Pmax = Vs*(Vs-Vz)/RsMin; % Max Input Power with minimum series resistance
   Pmin = Vs*(Vs-Vz)/RsMax; % Min Input Power with maximum series resistance
-  Pout = Vz * IL;
-  PeffMin = 100 * Pout / Pmax;
-  PeffMax = 100 * Pout / Pmin;
+  Pout = Vz * IL;          % Power Output
+  PeffMin = 100 * Pout / Pmax;  % Power Efficiency Minimum
+  PeffMax = 100 * Pout / Pmin;  % Power Efficiency Maximum
   
   disp('...');
   disp(['Maximum Series Resistor: ' num2str(RsMax)]);
