2801e0f69dc7e7990ac9de41841acb879cf2b674
harrimand <harrimand@gmail.com>
2019-10-21 22:12:44 -0700

    Updated Plots

--- circleSin.m
+++ circleSin.m
@@ -9,6 +9,7 @@ y2 = R2 * sin(t);
 R1z = zeros(1,length(t));
 
 figure('units','normalized','outerposition',[0 0 1 1]);
+set(gcf, 'Position', [.2, .2, .3, .3])
 while(1)
 subplot(1,2,1)
 hold on;
@@ -18,7 +19,8 @@ set(gca, 'xlim', [-R1*1.25, R1*1.25]);
 set(gca, 'ylim', [-R1*1.25, R1*1.25]);
 grid on
 
-    subplot(1,2,1)
+    AX1 = subplot(1,2,1);
+    % set(AX1, 'Position', [.1, .1, .4, .8])
     anno11 = '{\fontsize{24}\pi\fontsize{18}/2}';
     text(-.3, 5.25, anno11);
     anno11 = '{\fontsize{24}\pi}';
@@ -28,13 +30,13 @@ grid on
     anno11 = '{\fontsize{18}2\fontsize{24}\pi}';
     text(5.3, 0, anno11);
     plot(x1, y1, 'Color', 'b', 'LineWidth', 2);
-    plot(x1, y1, 'Color', 'm');
-    line([R1z(1), x1(1)],[R1z(1), y1(1)], 'Color', 'r', 'LineWidth', 2);
+    L1 = line([R1z(1), x1(1)],[R1z(1), y1(1)], 'Color', 'r', 'LineWidth', 3);
+    L2 = line([x1(2), R1*1.25],[y1(2), y1(2)], 'Color', 'k', 'LineWidth', 1);
 
-    
-    
-    subplot(1,2,2);
+    AX2 = subplot(1,2,2);
+    % set(AX2, 'Position', [.5, .1, .4, .8])
     line([t(1), t(2)],[y1(1),y1(2)], 'Color', 'r', 'LineWidth', 2);
+    LS2 = line([0, t(2)], [y1(2), y1(2)],  'Color', 'k', 'LineWidth', 1);
     title('Sine Wave', 'FontSize',22 )
     set(gca, 'xlim', [0, t(end)]);
     set(gca, 'ylim', [-R1*1.25, R1*1.25]);
@@ -42,18 +44,15 @@ grid on
 
     for L = 2:length(t)-1
         subplot(1,2,1)
-        lastLine = findobj(gca, 'Type', 'line');
-        delete(lastLine(1));
-        lastLine = findobj(gca, 'Type', 'line');
-        delete(lastLine(1));
-        line([R1z(L), x1(L)],[R1z(L), y1(L)], 'Color', 'r', 'LineWidth', 2);
-        line([x1(L), R1*1.25],[y1(L), y1(L)], 'Color', 'k', 'LineWidth', 1);
+        rotate(L1, [0, 0, 1], 2)
+        set(L2, 'xdata', [x1(L), R1*1.25], 'ydata', [y1(L), y1(L)])
+        drawnow()
+        
         subplot(1,2,2)
-        lastLine = findobj(gca, 'Type', 'line');
-        delete(lastLine(1));
         line([t(L-1), t(L)],[y1(L-1), y1(L)], 'Color', 'r', 'LineWidth', 2);
-        line([0, t(L)], [y1(L), y1(L)],  'Color', 'k', 'LineWidth', 1);
+        set(LS2, 'xdata', [0, t(L)], 'ydata', [y1(L), y1(L)])
         pause(.02);
+        drawnow()
     end
     pause;
     axesHandlesToChildObjects = findobj(gca, 'Type', 'line');
